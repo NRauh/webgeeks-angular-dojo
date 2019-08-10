c80caf9a3a5c3e4d158341bc49ebb524995f8e36
Nate Rauh <nate.rauh@interactiveshift.com>
2019-08-10 16:09:31 -0500

    Seed data

--- packages/api/db/seeds.rb
+++ packages/api/db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+def create_game
+  game = Game.create!(
+    name: Faker::Lorem.sentence,
+    active: Faker::Boolean.boolean
+  )
+
+  4.times { create_note(game) }
+end
+
+def create_note(game)
+  game.notes.create!(
+    name: Faker::Lorem.sentence,
+    body: Faker::Lorem.paragraphs(number: 7, supplemental: true).join("\n")
+  )
+end
+
+4.times { create_game }
